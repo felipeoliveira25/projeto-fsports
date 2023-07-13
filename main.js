44d26307676004059829c3822bc4493227ac50f0
felipeoliveira25 <felipeoferreira.25@gmail.com>
2023-07-13 04:03:04 -0300

    commit das novas funcionalidades da aplicação, como: a possibilidade de adicionar novas postagens com foto e texto.

--- main.js
+++ main.js
@@ -4,8 +4,69 @@
     
 // });
 
-const fileSelector = document.getElementById('file-selector');
-fileSelector.addEventListener('change', (event) => {
-  const fileList = event.target.files;
-  console.log(fileList);
-});
+// const fileSelector = document.getElementById('file-selector');
+// fileSelector.addEventListener('change', (event) => {
+//   const fileList = event.target.files;
+//   console.log(fileList);
+// });
+
+const form = document.getElementById('novaPostagem');
+// const posts = localStorage.getItem("posts") || []
+
+
+form.addEventListener("submit", (evento)=>{
+    evento.preventDefault()
+
+    const texto = evento.target.elements['texto'].value
+    const imagem = evento.target.elements['imagem'].files[0];
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      criaPostagem(event.target.result, texto);
+    }
+    reader.readAsDataURL(imagem);
+  
+    evento.target.reset();
+    
+    // criaPostagem(imagem.value, texto.value)
+    // imagem.value = ""
+    // texto.value = ""
+    
+} )
+
+function criaPostagem (imagem, texto){
+    const novaDiv = document.createElement("div");
+    novaDiv.classList.add("noticia-principal")
+
+    const novaImagem = document.createElement("img");
+    novaImagem.src = imagem;
+
+    const novoTexto = document.createElement("section");
+    novoTexto.innerHTML = texto
+
+    novaDiv.appendChild(novaImagem);
+    novaDiv.appendChild(novoTexto);
+    // novaDiv.innerHTML += novoTexto
+
+    const postagens = document.getElementById("main")
+    postagens.appendChild(novaDiv)
+
+    
+    // const postagemAtual = {
+    //     "texto": texto,
+    //     "imagem": imagem
+    // }
+
+    // posts.push(postagemAtual)
+
+    // localStorage.setItem("posts", JSON.stringify(posts))
+
+    
+}
+
+// const botao = document.getElementById('botaoPostar');
+
+// botao.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     alert("olá mundo")
+// })
